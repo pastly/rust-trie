9d5bb85cd9e9a8981392a5afbe12a39962147c8e
Matt Traudt <sirmatt@ksu.edu>
2019-02-19 22:16:24 -0500

    Add Trieable trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
+use std::fmt::Debug;
+use std::clone::Clone;
+
 #[derive(Debug)]
 struct Trie<T>
-where T: std::fmt::Debug + std::clone::Clone
+where T: Debug + Clone + Trieable<T>
 {
     val: T,
     children: Vec<Self>,
 }
 
 impl<T> Trie<T>
-where T: std::fmt::Debug + std::clone::Clone
+where T: Debug + Clone + Trieable<T>
 {
     fn new(val: T) -> Self {
         Self{val: val, children: vec![]}
@@ -27,13 +30,40 @@ where T: std::fmt::Debug + std::clone::Clone
     }
 }
 
+trait Trieable<T>
+{
+    fn trie_split(&self) -> Vec<T>;
+    fn trie_join(vals: &[T]) -> T;
+}
+
+impl Trieable<String> for String
+{
+    fn trie_split(&self) -> Vec<Self> {
+        self.chars().map(|c| c.to_string()).collect()
+    }
+
+    fn trie_join(vals: &[String]) -> String {
+        vals.join("")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Trie;
+    use super::Trieable;
     #[test]
     fn foo() {
-        let mut t = Trie::new("A");
-        t.insert_recursive(&["B", "C", "D"]);
+        let mut t = Trie::new("A".to_string());
+        t.insert_recursive(&["B".to_string(), "C".to_string(), "D".to_string()]);
+        println!("{:?}", t);
+        assert!(true);
+    }
+
+    #[test]
+    fn bar() {
+        let s = String::from("ABC");
+        println!("{:?}", s.trie_split());
+        let t = String::trie_join(&["D".to_string(), "E".to_string(), "F".to_string()]);
         println!("{:?}", t);
         assert!(false);
     }
