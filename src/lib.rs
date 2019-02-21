bce1aa0c63b1f0af79c02997ae78b700fc3eff1c
Matt Traudt <sirmatt@ksu.edu>
2019-02-20 19:00:13 -0500

    Near total rewrite. It's better this time. Trust me.

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,78 @@
-use std::fmt::Debug;
 use std::clone::Clone;
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::hash::Hash;
 
 #[derive(Debug)]
-struct Trie<T>
-where T: Debug + Clone + Trieable<T>
+struct Trie<K, V>
+where
+    K: Eq + Hash + Debug + Clone,
+    V: Debug + Clone,
 {
-    val: T,
-    children: Vec<Self>,
+    val: Option<V>,
+    children: HashMap<K, Trie<K, V>>,
 }
 
-impl<T> Trie<T>
-where T: Debug + Clone + Trieable<T>
+impl<K, V> Trie<K, V>
+where
+    K: Eq + Hash + Debug + Clone,
+    V: Debug + Clone,
 {
-    fn new(val: T) -> Self {
-        Self{val: val, children: vec![]}
-    }
-    fn insert(&mut self, val: T) {
-        self.children.push(Self::new(val))
+    fn new(val: Option<V>) -> Self {
+        Self {
+            val,
+            children: HashMap::new(),
+        }
     }
 
-    fn insert_recursive(&mut self, vals: &[T]) {
-        if vals.len() < 1 {
+    fn insert(&mut self, keys: &[K], val: V) {
+        if keys.is_empty() {
+            assert!(
+                self.val.is_none(),
+                "Tried to insert into Trie where value already exists"
+            );
+            self.val = Some(val);
             return;
         }
-        let (first, remaining) = vals.split_first().unwrap();
-        let mut new = Trie::new(first.clone());
-        new.insert_recursive(remaining);
-        self.children.push(new)
-    }
-}
-
-trait Trieable<T>
-{
-    fn trie_split(&self) -> Vec<T>;
-    fn trie_join(vals: &[T]) -> T;
-}
-
-impl Trieable<String> for String
-{
-    fn trie_split(&self) -> Vec<Self> {
-        self.chars().map(|c| c.to_string()).collect()
+        assert!(!keys.is_empty());
+        let (first, remaining) = keys.split_first().unwrap();
+        if self.children.contains_key(first) {
+            self.children.get_mut(first).unwrap().insert(remaining, val);
+        } else {
+            let mut new = Trie::new(None);
+            new.insert(remaining, val);
+            self.children.insert(first.clone(), new);
+        }
     }
 
-    fn trie_join(vals: &[String]) -> String {
-        vals.join("")
+    fn fetch(&self, keys: &[K]) -> Option<V> {
+        if keys.is_empty() {
+            return self.val.clone();
+        }
+        assert!(!keys.is_empty());
+        let (first, remaining) = keys.split_first().unwrap();
+        if self.children.contains_key(first) {
+            self.children[first].fetch(remaining)
+        } else {
+            None
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Trie;
-    use super::Trieable;
     #[test]
     fn foo() {
-        let mut t = Trie::new("A".to_string());
-        t.insert_recursive(&["B".to_string(), "C".to_string(), "D".to_string()]);
+        let mut t: Trie<u8, bool> = Trie::new(None);
+        t.insert(&[1, 2, 3], true);
+        t.insert(&[1, 2, 4], true);
         println!("{:?}", t);
-        assert!(true);
-    }
-
-    #[test]
-    fn bar() {
-        let s = String::from("ABC");
-        println!("{:?}", s.trie_split());
-        let t = String::trie_join(&["D".to_string(), "E".to_string(), "F".to_string()]);
-        println!("{:?}", t);
+        let v1 = t.fetch(&[1]);
+        println!("{:?}", v1);
+        let v2 = t.fetch(&[1, 2, 3]);
+        println!("{:?}", v2);
         assert!(false);
     }
 }
